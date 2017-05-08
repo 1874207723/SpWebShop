0c334920ad9b14739f2efdca90fe915af379a70f
1874207723 <1874207723@qq.com>
2017-05-08 09:08:37 +0800

    5-8修改了路径

--- application/config.php
+++ application/config.php
@@ -140,19 +140,19 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__PUBLIC__' => './',
-        '__STATIC__' => './static',
+        '__PUBLIC__' => rtrim(str_replace('\\','/',ROOT_PATH),'/').'/public',
+        '__STATIC__' => rtrim(str_replace('\\','/',ROOT_PATH),'/').'/public/static',
     ],
     // 默认跳转页面对应的模板文件
-    'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
-    'dispatch_error_tmpl'    => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
+    'dispatch_success_tmpl'  => str_replace('\\','/',THINK_PATH) . 'tpl' . DS . 'dispatch_jump.tpl',
+    'dispatch_error_tmpl'    => str_replace('\\','/',THINK_PATH) . 'tpl' . DS . 'dispatch_jump.tpl',
 
     // +----------------------------------------------------------------------
     // | 异常及错误设置
     // +----------------------------------------------------------------------
 
     // 异常页面的模板文件
-    'exception_tmpl'         => THINK_PATH . 'tpl' . DS . 'think_exception.tpl',
+    'exception_tmpl'         => str_replace('\\','/',THINK_PATH) . 'tpl' . DS . 'think_exception.tpl',
 
     // 错误显示信息,非调试模式有效
     'error_message'          => '页面错误！请稍后再试～',
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,10 +2,40 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Request;
+use think\Image;
 class Index extends Controller
 {
     public function index()
     {
+        dump('http://www.wntp.com/');
+    	dump(DS);
+    	dump(THINK_PATH);//框架系统目录
+    	dump(ROOT_PATH);//框架应用根目录
+    	dump(APP_PATH);//应用目录（默认为
+    	dump(CONF_PATH);//应用目录（默认为
+    	dump(VENDOR_PATH);//第三方类库目录（默认为
+    	dump(EXTEND_PATH);//扩展类库目录（默认为
+    	dump(11);//框架系统目录
+    	dump(CONF_PATH);//框架系统目录\
+
+    	echo '<hr />';
+    	$request = Request::instance();
+		echo '请求方法：' . $request->method() . '<br/>';
+		echo '资源类型：' . $request->type() . '<br/>';
+		echo '访问地址：' . $request->ip() . '<br/>';
+		echo '是否AJax请求：' . var_export($request->isAjax(), true) . '<br/>';
+		echo '请求参数：';
+		dump($request->param());
+		echo '请求参数：仅包含name';
+		dump($request->only(['name']));
+		echo '请求参数：排除name';
+		dump($request->except(['name']));
+		dump(input('get.'));
+
+        //$image = \think\Image::open();
+        //var_dump($image);
         return $this->fetch();
     }
 }
+ 
