ecd007a696c94b26eda03fea772ea3cf931189ac
junliezhixue <1412279986@qq.com>
2017-05-17 23:12:58 +0800

    列表页改进中

--- application/index/controller/Shop.php
+++ application/index/controller/Shop.php
@@ -28,13 +28,103 @@
  */
 
 namespace app\index\controller;
+use think\Db;
 
 class Shop extends Base
 {
     public function list()
     {
-    	$this->assign('title' ,'Q-Buy 商品列表');
+        $cateid = request()->param()['cateid'];
+        //查询板块分类，1,2,3级。
+        $level1 = Db::table('qb_goods_cate')->where('level', 1)->order('sort_order desc')->select();
+        foreach($level1 as $value) {
+            $data = Db::table('qb_goods_cate')->where('parent_id', $value['id'])->select();
+            $dataLimit = Db::table('qb_goods_cate')->where('parent_id', $value['id'])->limit(4)->select();
+            $level2[$value['id']] = $data;
+            $level2Limit[$value['id']] = $dataLimit;
+            foreach($data as $value2) {
+                $data2 = Db::table('qb_goods_cate')->where('parent_id', $value2['id'])->limit(5)->select();
+                $level3[$value2['id']] = $data2;
+            }
+        }
+
+        //查询头部板块推荐。
+        $hotCate = Db::table('qb_goods_cate')->where('is_hot', 1)->where('level', 'in', '2,3')->limit(8)->select();
+
+        //判断是id为几级分类。
+        $idCheck = Db::table('qb_goods_cate')->field('level,name,id,parent_id,parent_id_path')->where('id', $cateid)->find();
+
+        //取得父级id，name。
+        if ($idCheck) {
+            $parent = explode('_', $idCheck['parent_id_path']);
+            if (!empty($parent[1])) {
+                $parent1 = Db::table('qb_goods_cate')->field('id,name')->where('id', $parent[1])->find();
+                $parentData1 = Db::table('qb_goods_cate')->field('id,name')->where('parent_id', $parent1['id'])->select();
+                if (!empty($parentData1)) {
+                    foreach ($parentData1 as $pd1first) {
+                        $checkData1[$pd1first['id']] = $pd1first['name'];
+                        $$pd1second = Db::table('qb_goods_cate')->field('id,name')->where('parent_id', $pd1first['id'])->select();
+                        if (!empty($pd1second)) {
+                            foreach ($pd1second as $pd1third) {
+                                $checkData1[$pd1third['id']] = $pd1third['name'];
+                            }
+                        }
+                    }
+                    $checkData[1] = $checkData1;
+                }
+            }
+            if (!empty($parent[2])) {
+                $parent2 = Db::table('qb_goods_cate')->field('id,name')->where('id', $parent[2])->find();
+                $parentData2 = Db::table('qb_goods_cate')->field('id,name')->where('parent_id', $parent2['id'])->select();
+                if (!empty($parentData2)) {
+                    foreach ($parentData2 as $pd2first) {
+                       $checkData2[$pd2first['id']] = $pd2first['name'];
+                    }
+                    $checkData[2] = $checkData2;
+                }
+            }
+            if (!empty($parent[3])) {
+                $parent3 = Db::table('qb_goods_cate')->field('id,name,parent_id_path')->where('id', $parent[3])->find();
+                $checkData3 = $checkData2;
+                $checkData[3] = $checkData3;
+            }
+
+            $checkReco = $checkData[$idCheck['level']];
+
+            //查询本品牌热销
+            $recommendData = Db::table('qb_goods')->where('cat_id', 'in', array_keys($checkReco))->where('is_recommend', 1)->select();
+
+            //查询大家都在选
+            $hotData = Db::table('qb_goods')->where('cat_id', 'in', array_keys($checkReco))->where('is_hot', 1)->select();
+
+            //查询商品
+            $goods = Db::table('qb_goods')->field('goods_id,goods_name,comment_count,market_price,shop_price,original_img')->where('cat_id', 'in', array_keys($checkReco))->paginate(12);
+
+            //查询推荐商品
+            $recommendGoods = Db::table('qb_goods')->field('goods_id,goods_name,comment_count,market_price,shop_price,original_img')->where('cat_id', 'in', array_keys($checkReco))->where('is_recommend', 1)->paginate(3);
+
+             //查询品牌
+            $brandData = Db::table('qb_brand')->field('logo')->where('cat_id',$idCheck['id'])->order('sort desc')->limit(18)->select();
+            $brandAll = Db::table('qb_brand')->field('id, name')->order('sort asc')->select();
+
+            $this->assign([
+                'checkReco' => $checkReco,
+                'hotData' => $hotData,
+                'recommendData' => $recommmendData,
+                'brandData' => $brandData,
+                'brandAll' => $brandAll,
+                'hotCate' => $hotCate,
+                'goods' => $goods,
+                'recommendGoods' => $recommendGoods
+                ]);
+        }
         return $this->fetch();
+    }
+
+    public function checklevel($data)
+    {
+        $level2 = Db::table('qb_goods_cate')->where('parent_id', $data)->limit(5)->select();
+        return json($level2);
     }
 
     public function goodsinfo ()
@@ -42,7 +132,6 @@ class Shop extends Base
         return $this->fetch();
     }
 
-   
-  
+
+
 }
- 
