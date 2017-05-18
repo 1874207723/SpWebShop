9c74a937c4ccc32450b639413c7a08490205e1aa
junliezhixue <1412279986@qq.com>
2017-05-18 09:17:07 +0800

    补全

---
+++ application/index/model/GoodsCate.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @Author: Marte
+ * @Date:   2017-05-17 13:59:42
+ * @Last Modified by:   Marte
+ * @Last Modified time: 2017-05-17 14:01:10
+ */
+namespace app\index\model;
+use think\Model;
+use think\Db;
+class GoodsCate extends Model
+{
+
+}
